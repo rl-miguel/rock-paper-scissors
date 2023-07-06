b8c02fef2b746e7ffda16976aceece4946c3a71d
rl-miguel <ruelmiguel@protonmail.com>
2023-07-06 15:23:33 +0100

    Add promptPlayer for visitor's game input

--- myscripts.js
+++ myscripts.js
@@ -2,10 +2,12 @@ const a="Rock";
 const b="Paper";
 const c="Scissors";
 let result="";
+let playerResult=""
 
 let computerSelect = Math.random();
 
 console.log(computerSelect); // this shows result of Math.random 
+function play(a,b,c){
 if(computerSelect <=0.3333333333333333333333333333333333333333333333333){
     result= a;
 } else if(computerSelect >=0.6666666666666666666666666666666666666666666666666){
@@ -14,4 +16,23 @@ if(computerSelect <=0.3333333333333333333333333333333333333333333333333){
     result= b;
 }
 
-console.log(result)
+console.log(result);
+return result;
+}
+
+play(a,b,c)
+
+
+
+function promptPlayer(question,a,b,c){
+playerChoice= prompt("Rock, Paper or Scissors?")
+console.log(playerChoice);
+}
+
+
+
+promptPlayer()
+
+
+
+
