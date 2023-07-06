7314e206249ea5166489b978dc64495e590db669
rl-miguel <ruelmiguel@protonmail.com>
2023-07-06 14:29:38 +0100

    Finish random selection for computer choice

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,14 @@
 const a="Rock";
 const b="Paper";
 const c="Scissors";
-let computerSelect=0;
 
-function getComputerChoice(a,b,c){
- computerSelect = Math.random();
+let computerSelect = Math.random();
 
-
-
+console.log(computerSelect); // this shows result of Math.random 
+if(computerSelect <=0.3333333333333333333333333333333333333333333333333){
+    console.log(a);
+} else if(computerSelect >=0.6666666666666666666666666666666666666666666666666){
+    console.log(c)
+} else{
+    console.log(b)
 }
